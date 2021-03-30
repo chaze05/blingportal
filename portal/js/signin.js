9a7a120ffa460c93e1f0f734903f84fab11d4257
Arif <arif@bling.cloud>
2021-03-30 19:51:25 +0530

    businesslist dashboard

--- portal/js/signin.js
+++ portal/js/signin.js
@@ -1,50 +1,52 @@
 //UI_URL= "http://localhost:8080/business/"
-UI_URL = "https://www.bling-center.com/business/"
+UI_URL = 'https://www.bling-center.com/business/';
 
-
-$(document).ready(function() {
-$("#loader").hide()
-$("#unauth").hide()
-$("#emailmissing").hide()
-$("#passwordmissing").hide()
+$(document).ready(function () {
+	$('#loader').hide();
+	$('#unauth').hide();
+	$('#emailmissing').hide();
+	$('#passwordmissing').hide();
 });
 
 function auth() {
-        console.log("Hello Malika")
-        missflag = false
-        if ($("#username")[0].value == "") {
-            missflag = true
-            $("#emailmissing").show()
-        }
+	console.log('Hello Malika');
+	missflag = false;
+	if ($('#username')[0].value == '') {
+		missflag = true;
+		$('#emailmissing').show();
+	}
 
-        if ($("#password")[0].value == "") {
-            missflag = true
-            $("#passwordmissing").show()
-        }
+	if ($('#password')[0].value == '') {
+		missflag = true;
+		$('#passwordmissing').show();
+	}
 
-        if (missflag) {
-            return
-        }
+	if (missflag) {
+		return;
+	}
 
-        $("#loader").show()
-        $("#passwordmissing").hide()
-        $("#emailmissing").hide()
-          $.ajax({
-            type: 'post',
-            url: UI_URL + 'login',
-            data: {username: $('#username')[0].value, password: $('#password')[0].value},
-            success: function (data) {
-             console.log(data)
-             console.log("Hello Mallika");
-             $("#loader").hide()
-             $("#unauth").hide()
-             window.location = "businesslist_v2.html";
-            },
-            error: function(err) {
-                console.log(err)
-                $("#loader").hide()
-                $("#unauth").show()
-            }
-          });
-    };
-
+	$('#loader').show();
+	$('#passwordmissing').hide();
+	$('#emailmissing').hide();
+	$.ajax({
+		type: 'post',
+		url: UI_URL + 'login',
+		data: {
+			username: $('#username')[0].value,
+			password: $('#password')[0].value,
+		},
+		success: function (data) {
+			console.log(data);
+			console.log('Hello Mallika');
+			$('#loader').hide();
+			$('#unauth').hide();
+			// window.location = 'businesslist_v2.html';
+			window.location = 'businessdata_lists.html';
+		},
+		error: function (err) {
+			console.log(err);
+			$('#loader').hide();
+			$('#unauth').show();
+		},
+	});
+}
